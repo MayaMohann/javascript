1d3b181b295e6b8309dc4a3948b77e87c5dcf9cd
MayaMohann <mayamohan2212@gmail.com>
2024-08-07 12:02:32 +0530

    updated

--- employeeDetails/employeeDetails.js
+++ employeeDetails/employeeDetails.js
@@ -1,13 +1,13 @@
 const employees = [
-    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000 },
-    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000 },
-    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000 },
-    { id: 4, name: 'Tom', age: 35, department: 'HR', salary: 45000 },
+    { id: 1, name: 'John Doe', age: 30, department: 'IT', salary: 50000, specialization: 'Python'},
+    { id: 2, name: 'Alice Smith', age: 28, department: 'HR', salary: 45000, specialization: 'Java'},
+    { id: 3, name: 'Bob Johnson', age: 35, department: 'Finance', salary: 60000, specialization: 'JavaScript'},
+    { id: 4, name: 'Tom', age: 35, department: 'HR', salary: 45000, specialization: 'Java' },
 
   ];
 
    // Function to display all employees
-   
+
     function displayEmployees(){
         const totalEmployees = employees.map((employee, index) =>
             `<p>${employee.id}: ${employee.name}: ${employee.department}: $${employee.salary} </p>`).join(" ");
@@ -36,3 +36,10 @@ const employees = [
   
         }
     }    
+
+    function findEmployeeBySpecialization(specializationEmp){
+        const specializationEmployee = employees.filter(employee => employee.specialization === specializationEmp)
+        const specializedEmployee = specializationEmployee.map((employee, index) =>
+            `<p>${employee.id}: ${employee.name}: ${employee.name} - ${employee.department} - $${employee.salary} - ${employee.specialization}</p>`).join('');
+        document.getElementById('employeesDetails').innerHTML = specializedEmployee;
+    }
